c7c468c3704bce9141e9f30840f3ab13ed11d351
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-10-29 20:52:55 +0530

    Update mainfuc.py

--- plugins/mainfuc.py
+++ plugins/mainfuc.py
@@ -84,7 +84,7 @@ async def upload(client,message):
 			title = v_[0].split('-')[1]
 			link  = v_[1].split('-')[1].replace(" ","")
 		except :
-			await message.reply_text('**==> Follow Below Formart To Upload Link To Your Pdisk Account** \n**• title** - <i>Title Of Your Post</i> \n**• link** - <i>Your File Link</i> \n**• thumb** - <i>Your Post Thumbnail Link<i> \n**==> For To Generate File Link And Images Links Follow Below Steps** \n• Send Image To Any Telegraph Uploader Bot For Permanent Link \n• Use @DirectLinkGeneratorBot For Instant File To Link Generation Or Use Index Link \n**==> About Speed Of Uploading** \n• Speed If Uploading Is Depends On File Size And Speed Of Link , If Your Using Instant Link Generation Bots Then Speed Of Uploading Will Be Too Slow , It's Better To Use Index Links For Speed Uploading',reply_to_message_id = message.message_id)
+			await message.reply_text('**==> Follow Below Format To Upload Link To Your Pdisk Account** \n**• title** - <i>Title Of Your Post</i> \n**• link** - <i>Your File Link</i> \n**• thumb** - <i>Your Post Thumbnail Link<i> \n**==> For To Generate File Link And Images Links Follow Below Steps** \n• Send Image To Any Telegraph Uploader Bot For Permanent Link \n• Use @DirectLinkGeneratorBot For Instant File To Link Generation Or Use Index Link \n**==> About Speed Of Uploading** \n• Speed If Uploading Is Depends On File Size And Speed Of Link , If Your Using Instant Link Generation Bots Then Speed Of Uploading Will Be Too Slow , It's Better To Use Index Links For Speed Uploading ',reply_to_message_id = message.message_id)
 			return
 		try:
 			thumb =  v_[2].split('-')[1].replace(" ","")
