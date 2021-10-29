6a385e1f6e4b4a0643d045335966410079fc01dd
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-10-29 22:12:18 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
     START_TEXT = """**Hai** {} , **I Am A Pdisk Url Uploading Bot**"""
     HELP_TEXT = """
-**==> Follow Below Format To Upload Link To Your Pdisk Account** \n\n**• title** - `Title Of Your Post` \n**• link** - `Your File Lin` \n**• thumb** - `Your Post Thumbnail Link` \n\n**==> For To Generate File And Images Links Follow Below Steps** \n\n• Send Image To Any Telegraph Uploader Bot For Permanent Link \n• Use @DirectLinkGeneratorBot For Instant File To Link Generation Or Use Index Link \n\n**==> About Speed Of Uploading** \n\n• Speed If Uploading Is Depends On File Size And Speed Of Link , If Your Using Instant Link Generation Bots Then Speed Of Uploading Will Be Too Slow , It's Better To Use Index Links For Speed Uploading**
+**==> Follow Below Format To Upload Link To Your Pdisk Account** \n\n**• title** - `Title Of Your Post` \n**• link** - `Your File Link` \n**• thumb** - `Your Post Thumbnail Link` \n\n**==> For To Generate File And Images Links Follow Below Steps** \n\n• Send Image To Any Telegraph Uploader Bot For Permanent Link \n• Use @DirectLinkGeneratorBot For Instant File To Link Generation Or Use Index Link \n\n**==> About Speed Of Uploading** \n\n• Speed If Uploading Is Depends On File Size And Speed Of Link , If Your Using Instant Link Generation Bots Then Speed Of Uploading Will Be Too Slow , It's Better To Use Index Links For Speed Uploading**
 """
     ABOUT_TEXT = """**• Bot Name** : **Pdisk Bot** \n**• Modified & Developed By** : **Garou** \n**• Version** : **Beta**"""
