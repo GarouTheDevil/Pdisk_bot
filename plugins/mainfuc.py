6cc47fee954f40357c783cb9ba9296f69323e50b
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-10-29 23:24:01 +0530

    Update mainfuc.py

--- plugins/mainfuc.py
+++ plugins/mainfuc.py
@@ -59,8 +59,12 @@ async def get_uptime(client, m: Message):
 
 @Client.on_message(filters.private & filters.command(['connect']))
 async def connect(client,message):
-	await message.reply_text('Send Your <a herf="https://www.pdisks.com/use-api">API_KEY</a> From Pdisks.net \n\n**Use This Link** : https://www.pdisks.com/use-api', reply_to_message_id=message.message_id, parse_mode='Html', reply_markup=ForceReply(True))
-
+	await message.reply_text(
+  'Send Your <a herf="https://www.pdisks.com/use-api">API_KEY</a> From Pdisks.net \n\n**Use This Link** : https://www.pdisks.com/use-api', 
+  reply_to_message_id=message.message_id, 
+  parse_mode='html', 
+  reply_markup=ForceReply(True)
+)
 @Client.on_message(filters.private & filters.reply)
 async def api_connect(client,message):
     if (message.reply_to_message.reply_markup) and isinstance(message.reply_to_message.reply_markup, ForceReply):
