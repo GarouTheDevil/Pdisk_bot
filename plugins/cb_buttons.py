f8946185b1957cdd69fa067a3325958942c2c12f
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-10-29 20:17:40 +0530

    Initial commit

---
+++ plugins/cb_buttons.py
@@ -0,0 +1,52 @@
+from translation import Translation
+
+import pyrogram
+
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from plugins.help_text import help, start, about
+
+@Client.on_callback_query()
+async def button(bot, update):
+      if update.data == "start":
+        await update.message.edit_text(
+            text=Translation.START_TEXT.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                        InlineKeyboardButton("HELP", callback_data = "help"),
+                        InlineKeyboardButton("ABOUT", callback_data = "about"),
+                        InlineKeyboardButton("CLOSE", callback_data = "close")
+                ]
+            ]
+        ))
+      if update.data == "help":
+        await update.message.edit_text(
+            text=Translation.HELP_TEXT,
+            disable_web_page_preview = True,
+            reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('BACK', callback_data = "start")
+                ]
+            ]
+        ))
+      if update.data == "about":
+       await update.message.edit_text(
+           text=Translation.ABOUT_TEXT,
+            disable_web_page_preview = True,
+            reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('BACK', callback_data = "start")
+                ]
+            ]
+        ))
+      if update.data == "close":
+        await update.message.delete()
+        try:
+            await update.message.reply_to_message.delete()
+        except:
+            pass
+   
---
+++ translation.py
@@ -0,0 +1,4 @@
+class Translation(object):
+    START_TEXT = """**Hai** {} , **I Am A Pdisk Url Uploading Bot**"""
+    HELP_TEXT = """**Updating**"""
+    ABOUT_TEXT = """**Updating**"""
