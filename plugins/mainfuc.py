0887acdd947f5278f45f90ec9f1ce9c6812c307b
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-10-30 09:15:17 +0530

    Update mainfuc.py

--- plugins/mainfuc.py
+++ plugins/mainfuc.py
@@ -91,8 +91,8 @@ async def upload(client,message):
 		except :
 			await message.reply_text('**==> Follow Below Steps To Upload Link To Account** \n\n**title** - `Title Of Post` \n**link** - `File Link` \n**thumb** - `Post Thumbnail Link` \n\n**Use Cammand** : /help **For More Details**',reply_to_message_id = message.message_id)
 			return
-	        try:
-                        cover_url = v_[2].split('_')[1].replace(" ","")
+		try:
+			cover_url = v_[2].split('-')[1].replace(" ","")
 		except:
 			cover_url = None
 		if thumb:
