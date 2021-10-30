409c18876a08f3c850232560e5167e0bc8c107b5
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-10-30 08:29:26 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,9 +1,6 @@
 class Translation(object):
     START_TEXT = """**Hai** {} , **I Am A Pdisk Url Uploading Bot**"""
     HELP_TEXT = """
-**==> Follow Below Steps To Login To Pdisk Account** \n\nUse Cammand : /connect {api key} , To Login To Desired Account \n**• Example** : /connect **01234ancdefghijkl**
-\n\n**==> Follow These Steps To Upload Link To Account** \n\n**title** - `Title Of Post` \n**link** - `File Link` \n**thumb** - `Post Thumbnail Link` 
-\n\n**==> For To Generate File And Images Links Follow Below Steps** \n\n• Send Image To Any Telegraph Uploader Bot For Permanent Link \n• Use @DirectLinkGeneratorBot For Instant File To Link Generation Or Use Index Link 
-\n\n**==> About Speed Of Uploading** \n\n• Speed Of Uploading Is Depends On File Size And Speed Of Link , If Your Using Instant Link Generation Bots Then Speed Of Uploading Will Be Too Slow , It's Better To Use Index Links For Speed Uploading**
+**==> Follow Below Steps To Login To Pdisk Account** \n\nUse Cammand : /connect {api key} , To Login To Desired Account \n**• Example** : /connect **01234anx** \n\n**==> Follow Below Steps To Upload Link To Account** \n\n**title** - `Title Of Post` \n**link** - `File Link` \n**thumb** - `Post Thumbnail Link` \n\n**==> For To Generate File And Images Links Follow Below Steps** \n\n• Send Image To Any Telegraph Uploader Bot For Permanent Link \n• Use @DirectLinkGeneratorBot For Instant File To Link Generation Or Use Index Link \n\n**==> About Speed Of Uploading** \n\n• Speed Of Uploading Is Depends On File Size And Speed Of Link , If Your Using Instant Link Generation Bots Then Speed Of Uploading Will Be Too Slow , It's Better To Use Index Links For Speed Uploading**
 """
     ABOUT_TEXT = """**• Bot Name** : **Pdisk Bot** \n**• Modified & Developed By** : **Garou** \n**• Version** : **Beta**"""
