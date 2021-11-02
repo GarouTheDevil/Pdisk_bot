ba82e2d1374cf2ce744072b5c40303b19af9df81
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-11-02 22:57:30 +0530

    Update mainfuc.py

--- plugins/mainfuc.py
+++ plugins/mainfuc.py
@@ -109,7 +109,7 @@ async def upload(client,message):
 			res = pdisk_url(api_key,link,title)
 			try:
 				id = res['data']['item_id']
-				await message.reply_text(f'**• Title** : {title} \n**• URL** : https://cofilink.com/share-video?videoid={id} \n**• Thumbnail** : `{thumb}` \n\n**Post Will Be Uploaded With In Hour**',reply_to_message_id = message.message_id)
+				await message.reply_text(f'**• Title** : {title} \n**• URL** : https://cofilink.com/share-video?videoid={id} \n**• Thumbnail** : `{thumb}` \n\n**Post Will Be Uploaded With In Hour**',reply_to_message_id = message.message_id,reply_markup=InlineKeyboardMarkup([ [ InlineKeyboardButton("Pdisk Url Status", url="https://www.pdisks.com/home") ], [InlineKeyboardButton("Uploaded Video Url" , url="https://cofilink.com/share-video?videoid={id}"] ] )
 			except:
 				e = res['msg']
 				await message.reply_text(f"**Error** : ```{e}```",reply_to_message_id = message.message_id)
